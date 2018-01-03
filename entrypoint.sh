5195a8ad62eaf6d4e693af3b6728929d7888957d
John van Zantvoort <john@vanzantvoort.org>
2018-01-03 18:52:04 +0000

    initial version of the builder scripts

---
+++ entrypoint.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+#===============================================================================
+#
+#         FILE:  entrypoint.sh
+#
+#        USAGE:  entrypoint.sh
+#
+#  DESCRIPTION:  Bash script
+#
+#      OPTIONS:  ---
+# REQUIREMENTS:  ---
+#         BUGS:  ---
+#        NOTES:  ---
+#       AUTHOR:  John van Zantvoort, John@------------.org
+#      COMPANY:  none
+#      CREATED:  18-Jul-2017
+#===============================================================================
+
+declare -r CONST_SCRIPTPATH="$(readlink -f $0)"
+declare -r CONST_SCRIPTNAME="$(basename $CONST_SCRIPTPATH .sh)"
+declare -r CONST_SCRIPTDIR="$(dirname $CONST_SCRIPTPATH)"
+declare -r CONST_FACILITY="local0"
+declare -xr LANG="C"
+
+function logging()
+{
+  local priority="$1"; shift
+  logger -p ${CONST_FACILITY}.${priority} -i -s -t "${CONST_SCRIPTNAME}" -- "${priority} $@"
+}
+
+function logging_err()
+{
+  logging "err" "$@"
+}
+
+function logging_info()
+{
+  logging "info" "$@"
+}
+
+function script_exit()
+{
+  local STRING="$1"
+  local RETV="${2:-0}"
+  if [ "$RETV" = "0" ]
+  then
+    logging_info "$STRING"
+  else
+    logging_err "$STRING"
+  fi
+  exit $RETV
+}
+
+#------------------------------------------------------------------------------#
+#                                    Main                                      #
+#------------------------------------------------------------------------------#
+
+logging_info "start"
+
+# setup path
+#---------------------------------------
+sphinx-build -b html -d /code/tmp/doctrees sphinxdoc /webroot || script_exit "failed" 1
+
+rm -rf /code/tmp/doctrees || script_exit "failed to cleanup" 3
+
+script_exit "end"
+#------------------------------------------------------------------------------#
+#                                  The End                                     #
+#------------------------------------------------------------------------------#
