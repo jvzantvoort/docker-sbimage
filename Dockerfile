c948e2319a2d37d09544fbd357cd361a85dc83e0
John van Zantvoort <john.van.zantvoort@proxy.nl>
2019-07-23 17:08:27 +0200

    - Updated to fix security issues - Added puppet-pygments-lexer-0.0.1.tar.gz - Updated scripts

--- Dockerfile
+++ Dockerfile
@@ -6,12 +6,16 @@ LABEL \
 
 # add the python requirements file
 ADD requirements.txt /tmp/requirements.txt
+ADD puppet-pygments-lexer-0.0.1.tar.gz /tmp
 
 # install required debian packages and
 # run pip to install the requirements
 RUN apt-get update && apt-get install -y libldap2-dev libsasl2-dev rsync && \
 pip install -r /tmp/requirements.txt && \
 sed -i 's,sphinx.application,sphinx.errors,g' /usr/local/lib/python3.6/site-packages/sphinxcontrib/googleanalytics.py
+RUN cd /tmp/puppet-pygments-lexer-0.0.1; \
+    python setup.py install; \
+    cd /tmp
 
 WORKDIR /code
 VOLUME ["/webroot"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -19,13 +19,14 @@
 declare -r CONST_SCRIPTPATH="$(readlink -f $0)"
 declare -r CONST_SCRIPTNAME="$(basename $CONST_SCRIPTPATH .sh)"
 declare -r CONST_SCRIPTDIR="$(dirname $CONST_SCRIPTPATH)"
-declare -r CONST_FACILITY="local0"
+declare -r CONST_PWD="${PWD}"
 declare -xr LANG="C"
 
 function logging()
 {
   local priority="$1"; shift
-  logger -p ${CONST_FACILITY}.${priority} -i -s -t "${CONST_SCRIPTNAME}" -- "${priority} $@"
+  local lineno="$1"; shift
+  printf "%s:%-4d [%s] %s\n" "${CONST_SCRIPTNAME}" "${lineno}" "${priority}" "$@"
 }
 
 function logging_err()
@@ -40,40 +41,91 @@ function logging_info()
 
 function script_exit()
 {
-  local STRING="$1"
-  local RETV="${2:-0}"
+  local STRING="$1"; shift
+  local lineno="$1"; shift
+  local RETV="${1:-0}"
   if [ "$RETV" = "0" ]
   then
-    logging_info "$STRING"
+    logging_info $LINENO "$STRING"
   else
-    logging_err "$STRING"
+    logging_err $LINENO "$STRING"
   fi
   exit $RETV
+}
+
+function cmd_start()
+{
+  local CMD=$1; shift
+  printf " start %-32s\n" "${CMD}" | tr ' ' '-'
+}
+
+function cmd_end()
+{
+  local CMD=$1; shift
+  printf " %33s end-\n\n" "${CMD}" | tr ' ' '-'
 }
 
 #------------------------------------------------------------------------------#
 #                                    Main                                      #
 #------------------------------------------------------------------------------#
 
-logging_info "start"
+logging_info $LINENO "start"
 
-UID=$1; shift
-GID=$1; shift
+G_UID=$1; shift
+G_GID=$1; shift
+VERSION=$1; shift
 
-# setup path
-#---------------------------------------
+VERSION=$(echo $VERSION|sed 's,.*\(........\)$,\1,')
+
+[[ -z "${G_UID}" ]] && G_UID="1001"
+[[ -z "${G_GID}" ]] && G_GID="1001"
+[[ -z "${VERSION}" ]] && VERSION="none"
+
+logging_info $LINENO "uid: ${G_UID}"
+logging_info $LINENO "gid: ${G_GID}"
+
+if [ -x "prebuild" ]
+then
+  cmd_start "prebuild"
+  ./prebuild /webroot
+  cmd_end "prebuild"
+fi
+
+STRING="sphinx-build"
+
+cmd_start "${STRING}"
 sphinx-build -b html \
   -d /tmp/doctrees \
   -N -q \
+  -A VERSION="${VERSION}" \
   sphinxdoc /webroot || script_exit "failed" 1
+cmd_end "${STRING}"
 
-chown -R "${UID}:${GID}" /webroot/*
 
+if [ -x "postprocessor" ]
+then
+  cmd_start "postprocessor"
+  ./postprocessor /webroot
+  cmd_end "postprocessor"
+fi
+
+cmd_start "fix ownership"
+chown --recursive "${G_UID}:${G_GID}" /webroot/*
+cmd_end "fix ownership"
+
+STRING="cleanup /tmp/doctrees"
+cmd_start "${STRING}"
 rm -rf /tmp/doctrees || script_exit "failed to cleanup" 3
+cmd_end "${STRING}"
 
-cp -rp  /webroot/* /output
+STRING="copy build results to output"
+cmd_start "${STRING}"
+pushd /webroot
+tar -cvf "${CONST_PWD}/output.tar.gz" .
+popd
+cmd_end "${STRING}"
 
-script_exit "end"
+script_exit $LINENO "end"
 #------------------------------------------------------------------------------#
 #                                  The End                                     #
 #------------------------------------------------------------------------------#
